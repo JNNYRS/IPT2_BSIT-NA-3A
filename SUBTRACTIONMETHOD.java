5a1d4d050650f8cbb0cf35179bf38ade4b040b90
Ruth Joy Benitez <159256666+ruthjoyyy@users.noreply.github.com>
2024-02-16 20:52:51 +0800

    Add files via upload
    upload subtract method

---
+++ SUBTRACTIONMETHOD.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class SUBTRACTIONMETHOD {
+
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Enter the first number: ");
+    int num1 = scanner.nextInt();
+    
+    System.out.print("Enter the second number: ");
+    int num2 = scanner.nextInt();
+    
+    int result = num1 - num2;
+    
+    System.out.println("The result of subtraction is: " + result);
+    
+    scanner.close();
+    }
+    
+}
